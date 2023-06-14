34514fbea2f4a73280b90c5e7b95096de72af1b5
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-14 11:21:48 +0200

    Add main class and plugin.yml

---
+++ plugin/src/main/java/fr/stelycube/stelytaxi/StelyTaxiPlugin.java
@@ -0,0 +1,13 @@
+package fr.stelycube.stelytaxi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+@SuppressWarnings("unused")
+public class StelyTaxiPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+}
