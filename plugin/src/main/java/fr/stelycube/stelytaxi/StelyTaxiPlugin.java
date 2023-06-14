bc7f2d709cac626628998090e78cb4733bfb0db4
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2023-06-14 12:12:27 +0200

    Add ConfigLoader

--- plugin/src/main/java/fr/stelycube/stelytaxi/StelyTaxiPlugin.java
+++ plugin/src/main/java/fr/stelycube/stelytaxi/StelyTaxiPlugin.java
@@ -1,13 +1,30 @@
 package fr.stelycube.stelytaxi;
 
+import fr.stelycube.stelytaxi.config.ConfigLoader;
 import fr.stelycube.stelytaxi.trait.TraitLoader;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @SuppressWarnings("unused")
 public class StelyTaxiPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        final Logger logger = getLogger();
+        final File dataFolder = getDataFolder();
+        final ConfigLoader configLoader = new ConfigLoader();
+
+        if (!configLoader.checkDataFolder(dataFolder)) {
+            logger.log(Level.WARNING, "Can not write in the directory : " + dataFolder.getAbsolutePath());
+            logger.log(Level.WARNING, "Disable the plugin");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
         final TraitLoader traitLoader = new TraitLoader();
         traitLoader.load();
     }
